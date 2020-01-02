dfad4b0add95604d52e1d50831cbcb83f56f6664
Maxim Kravchenko <maks.cross@gmail.com>
2020-01-02 15:30:36 +0200

    Add project files.

---
+++ cycles_tests/Source.cpp
@@ -0,0 +1,228 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	//Task1. Calculator with using do-while
+	/*int act, A, B, res;
+
+	do
+	{
+		cout << "The menu:\n";
+		cout << "1 for use +"<<endl;
+		cout << "2 for use -"<< endl;
+		cout << "3 for use *"<< endl;
+		cout << "4 for use /" << endl;
+		cout << "0 for exit.\n" << endl;
+
+		cout<<"Enter the action: ";
+		cin >> act;
+		
+		switch (act)
+		{
+		case 1:
+			cout << "Enter the first and second number: ";
+			cin >> A >> B;
+			res = A + B;
+			cout << res<<endl;
+			break;
+		case 2:
+			cout << "Enter the first and second number: ";
+			cin >> A >> B;
+			res = A - B;
+			cout << res << endl;
+			break;
+		case 3:
+			cout << "Enter the first and second number: ";
+			cin >> A >> B;
+			res = A * B;
+			cout << res << endl;
+			break;
+		case 4:
+			cout << "Enter the first and second number: ";
+			cin >> A >> B;
+			res = A / B;
+			cout << res << endl;
+			break;
+		case 0:
+			cout << "It's exit!!!"<<endl;
+			break;
+		default:
+			cout<<"You enter wrong number!"<<endl;
+			break;
+		}
+		
+
+	} while (act != 0);
+
+	cout << "Good bue!";*/
+
+	//=====================================================================================================
+	//Task2. Programm calculate the sum of numbers
+	/*int begin, end, sum;
+
+	cout << "Enter the first number: \n";
+	cin >> begin;
+	cout << "Enter the last number: \n";
+	cin >> end;
+	
+	sum = 0;
+
+	while (begin <= end) {
+		sum += begin;
+		begin++;
+	}
+
+	cout << "\nThe sum your diapasone is: " << sum;*/
+	
+	//=====================================================================================================
+	//Task3. Programm displays the set number of stars
+	/*int amount, i;
+
+	i = 0;
+
+	cout << "Enter the set number of stars: \n";
+	cin >> amount;
+	cout << endl;
+
+	while (i < amount) {
+		cout << '*';
+		i++;
+	}
+
+	cout << "\nYour stars is over.";*/
+
+	//=====================================================================================================
+	//Task4. Program output line of symbols. 
+	//The user selects the number of symbols, type of symbols and the vertical or horizontal line.
+
+	/*int count, i, type_line;
+	char symbol;
+
+	i = 0;
+
+	do
+	{	
+		cout << "Enter the symbol: " << endl;
+		cin >>symbol;
+			
+		cout << "\nEnter 1 for vertical line.\n";
+		cout << "Enter 0 for horizontal line.\n\n";
+		cin >> type_line;
+		
+		cout << "Enter the number of symbols(0 for exit): \n";
+		cin >> count;
+		
+		while (i<count)
+		{
+			i++;
+			if (type_line == 1){cout <<symbol<<"\n";}
+			else if(type_line == 0){cout << symbol;}
+			else { cout << "The wrong choice!"; }
+		}
+		cout << "\n\n";
+		i = 0;
+	} while (count != 0);
+	
+	cout << "\nYou choose exit!\n";
+	cout << "Ok. Bye!";*/
+	
+	//=====================================================================================================
+	//Task5. The program counts the sum of all odd integers.
+	/*int begin, i, end, sum;
+
+
+	cout << "Enter the begin and last number: \n";
+	cin >> begin >> end;
+
+	sum = 0;
+	i = begin;
+	i--;
+
+	while (i < end) {
+		i++;
+		if (i % 2 == 0) {
+			continue;
+		}
+		sum += i;
+	}
+	
+	cout << "Sum of numbers is " << sum;*/
+	
+	//=====================================================================================================
+	//Task6. The program for determining the factorial of a non-negative integer.
+	/*int factorial, begin, answer;
+	
+	begin = 0;
+	answer = 1;
+
+	cout << "Enter the factorial: ";
+	cin >> factorial;
+
+	while (factorial < 0) {
+		cout << "You must enter the positive integer!\n";
+		cin >> factorial;
+	}
+
+	do{
+		begin++;
+		answer *= begin;
+	}
+	while (begin < factorial);
+
+	cout << "\nFactorial of " << factorial << "! is " << answer;*/
+
+	//=====================================================================================================
+	//Task7. We have the length of the canvas L. We have to make pillows of length P. 
+	//The width of the pillows and the length of the canvas are equal. 
+	//Determine how many pillows we can make, multiplication and division not to use.
+
+	/*float canvas, pillow;
+	int count;
+
+	count = 0;
+
+	cout << "Enter the length of the canvas and pillow: \n";
+	cin >> canvas >> pillow;
+
+	while (canvas - pillow >= 0)
+	{
+		canvas -= pillow;
+		count++;
+	}
+
+	cout << "You can made " << count << " pillows";*/
+
+	//=====================================================================================================
+	//Task8. There are several boxes in warehouse. We have to clean the warehouse. 
+	//Cars take turns approaching the warehouse and pick up the boxes. Determine how much drove up to the warehouse.
+	int count, boxes, car_with_box, i;
+
+	count = 0;
+
+	cout << "Enter the number of boxes: ";
+	cin >> boxes;
+	i = boxes;
+	if (boxes > 0) {
+		while (i >= 0)
+		{
+			cout << "Enter the number of boxes that the car picked up: ";
+			cin >> car_with_box;
+			i -= car_with_box;
+			count++;
+		}
+		if (i <= 0) { cout << "\nThe boxes is over.\n"; }
+
+		cout << "\nAt the warehouse was: " << boxes<< " boxes.\n";
+		cout << "Were used: " << count<< " cars.\n";
+	}
+	else {
+		cout << "\nThere are no boxes at the warehous.";
+	}
+
+
+
+	cout << "\n\n";
+	system("pause");
+
+}
